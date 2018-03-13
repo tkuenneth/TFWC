d4225a0e13e4d0b8beed4213c30f951e3aca880c
Thomas Künneth <thomas.kuenneth@mathema.de>
2018-03-13 13:29:27 +0100

    work in progress

---
+++ react_native/index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import ComicImage from './ComicImage';
+
+AppRegistry.registerComponent('react_native', () => App);
+AppRegistry.registerComponent('react_native', () => ComicImage);
